36bddeb0283d0634fe6541e99d67a8d2bdd7e8b5
pedrofbo <pedrofelipebo@gmail.com>
2020-10-04 16:12:51 -0300

    addressed changes requested in the PR

--- chess engine/ponto_virada.py
+++ chess engine/ponto_virada.py
@@ -1,8 +1,10 @@
 
-def pontosVirada (scores):
+def pontosVirada (scores: list):
     pontos_virada = [0]
 
     for i in range(1, len(scores)):
+        #em jogadas com cheque, a engine troca o score por um contador de jogadas até acontecer um cheque mate
+        #neste caso, o score passa a ser None, e por isso é feita a checagem de ele ser inteiro
         if type(scores[i]) == int and type(scores[i-1]) == int:
             if abs(scores[i]) >= (abs(scores[i - 1]) + 150) and abs(scores[i - 1]) < 150:
                 pontos_virada.append(1)
